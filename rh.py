0e29049f38e6784ab3982801e0da5153f315f873
Дюдя <dauren.yermenov@gmail.com>
2023-07-25 15:08:37 +0600

    plot graph for kumbsa - no apparent trend

--- rh.py
+++ rh.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_excel('kumbsa.xlsx')
+df = pd.read_excel('rh.xlsx')
+
 df.index = df['Дата']
 df = df.drop(columns='Дата').T
 dates = [i.split()[0]+' '+i.split()[1][-2:] for i in df.index]
@@ -11,5 +12,5 @@ itogo = df['Итого по Столбцам'].to_frame()
 itogo['SMA'] = itogo['Итого по Столбцам'].rolling(3).mean()
 
 plt.plot(itogo)
-plt.xticks(rotation=45)
+plt.xticks(fontsize='xx-small',rotation=90)
 plt.show()
