06170af8fd356da460a86659d27ea0c632657590
Дюдя <dauren.yermenov@gmail.com>
2023-07-29 10:41:46 +0600

    donkey, need to back up

--- main.py
+++ main.py
@@ -3,39 +3,9 @@ from math import ceil
 import requests
 import json
 
-data = {
-  "ORTHO Vision":
-    {
-      "Резус-фактор":
-      [
-        {
-        "707135": 200,
-        "6904591": 3060
-        },
-        [
-          [21.3, 2.84],
-          [24.2, 1.72],
-          [26.7, 1.77],
-          [27.2, 2.33]
-        ]
-      ],
-
-      "Проба Кумбса":
-        [
-          {
-          "707300": 400,
-          "100-68": 200,
-          "6904591": 3060
-          },
-          []
-        ]
-    }
-}
-
-
-# url = 'https://raw.githubusercontent.com/yermenoved/olimp-calculator/main/data.json'
-# file = requests.get(url)
-# data = json.loads(file.text)
+url = 'https://raw.githubusercontent.com/yermenoved/olimp-calculator/main/data.json'
+file = requests.get(url)
+data = json.loads(file.text)
 
 analyzer = st.selectbox(
   'Выберите анализатор:',
