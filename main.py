c95aa9693036b18198e499b8fe88752d521e66d3
Дюдя <dauren.yermenov@gmail.com>
2023-07-29 10:40:54 +0600

    donkey, need to back up

--- main.py
+++ main.py
@@ -3,9 +3,39 @@ from math import ceil
 import requests
 import json
 
-url = 'https://raw.githubusercontent.com/yermenoved/olimp-calculator/main/data.json'
-file = requests.get(url)
-data = json.loads(file.text)
+data = {
+  "ORTHO Vision":
+    {
+      "Резус-фактор":
+      [
+        {
+        "707135": 200,
+        "6904591": 3060
+        },
+        [
+          [21.3, 2.84],
+          [24.2, 1.72],
+          [26.7, 1.77],
+          [27.2, 2.33]
+        ]
+      ],
+
+      "Проба Кумбса":
+        [
+          {
+          "707300": 400,
+          "100-68": 200,
+          "6904591": 3060
+          },
+          []
+        ]
+    }
+}
+
+
+# url = 'https://raw.githubusercontent.com/yermenoved/olimp-calculator/main/data.json'
+# file = requests.get(url)
+# data = json.loads(file.text)
 
 analyzer = st.selectbox(
   'Выберите анализатор:',
@@ -24,5 +54,4 @@ table = data[analyzer][test][0] #load data about this specific test
 for element in table.keys():
   st.write('Артикул реагента/расходника:', element, 'Необходимо заявить упаковок:', ceil(amount/table[element]))
 
-
-
+print(table, type(table))
